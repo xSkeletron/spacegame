c003a9d0ec3f329abcdce0a900ce91dcb7d2cd22
Robert <robertmskelton@gmail.com>
2017-02-06 22:09:02 -0800

    First commit: background and janky buttons

---
+++ Assets/FirstButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstButton : MonoBehaviour {
+    int value;
+    public string mineralName; // 1-3
+
+    void Start()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), mineralName))
+        {
+            GameObject variables = GameObject.Find("Main Camera");
+            Variables variableScript = variables.GetComponent<Variables>();
+            print(variableScript.redMinerals);
+        }
+
+    }
+}
---
+++ Assets/Variables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Variables : MonoBehaviour {
+    public int redMinerals;
+    public int blueMinerals;
+    public int greenMinerals;
+    public string mineralName;
+
+    // Use this for initialization
+    void Start () {
+        redMinerals = 0;
+        blueMinerals = 0;
+        greenMinerals = 0;
+
+    }
+
+    // Update is called once per frame
+    /*	void onGUI () {
+            GUI.Label(new Rect(10, 10, 100, 20), redMinerals.ToString());
+            GUI.Label(new Rect(50, 10, 100, 20), blueMinerals.ToString());
+            GUI.Label(new Rect(90, 10, 100, 20), greenMinerals.ToString());
+
+        }
+    */
+
+    
+}
